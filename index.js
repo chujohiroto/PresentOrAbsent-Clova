bcd2a246688a71e781e18cc70516eff77839f34b
iRosSoftware <iros0522@gmail.com>
2018-09-29 16:03:39 +0900

    port

--- index.js
+++ index.js
@@ -47,3 +47,10 @@ app.post('/bot', clovaMiddleware, clovaSkillHandler);
 
 // Or you can simply use `bodyParser.json()` to accept any request without verifying, e.g.,
 app.post('/bot', bodyParser.json(), clovaSkillHandler);
+
+const PORT = process.env.PORT || 8080;
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(PORT, function () {
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
