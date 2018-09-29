f5495ca2ae008f18d3ebdcb81e1be0b2c93e0dd3
iRosSoftware <iros0522@gmail.com>
2018-09-29 15:52:33 +0900

    first

---
+++ index.js
@@ -0,0 +1,49 @@
+const clova = require('@line/clova-cek-sdk-nodejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const clovaSkillHandler = clova.Client
+  .configureSkill()
+  .onLaunchRequest(responseHelper => {
+    responseHelper.setSimpleSpeech({
+      lang: 'ja',
+      type: 'PlainText',
+      value: '遅刻チェック',
+    });
+  })
+  .onIntentRequest(async responseHelper => {
+    const intent = responseHelper.getIntentName();
+    const sessionId = responseHelper.getSessionId();
+
+    switch (intent) {
+      case 'Clova.YesIntent':
+        // Build speechObject directly for response
+        responseHelper.setSimpleSpeech({
+          lang: 'ja',
+          type: 'PlainText',
+          value: 'はいはい',
+        });
+        break;
+      case 'Clova.NoIntent':
+        // Or build speechObject with SpeechBuilder for response
+        responseHelper.setSimpleSpeech(
+          clova.SpeechBuilder.createSpeechText('いえいえ')
+        );
+        break;
+    }
+  })
+  .onSessionEndedRequest(responseHelper => {
+    const sessionId = responseHelper.getSessionId();
+
+    // Do something on session end
+  })
+  .handle();
+
+const app = new express();
+const clovaMiddleware = clova.Middleware({ applicationId: "YOUR_APPLICATION_ID" });
+// Use `clovaMiddleware` if you want to verify signature and applicationId.
+// Please note `applicationId` is required when using this middleware.
+app.post('/bot', clovaMiddleware, clovaSkillHandler);
+
+// Or you can simply use `bodyParser.json()` to accept any request without verifying, e.g.,
+app.post('/bot', bodyParser.json(), clovaSkillHandler);
